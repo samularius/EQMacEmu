15ea3e8114e1cf242c54ccd748f578b8e02bd10b
E Spause <espause0703@gmail.com>
2024-07-23 10:23:50 -0400

    Additional verification to resolve an observed live crash.

--- zone/groups.cpp
+++ zone/groups.cpp
@@ -358,8 +358,8 @@ void Group::SendHPPacketsFrom(Mob *member)
 
 //updates a group member's client pointer when they zone in
 //if the group was in the zone already
-bool Group::UpdatePlayer(Mob* update){
-
+bool Group::UpdatePlayer(Mob* update)
+{
 	VerifyGroup();
 
 	uint32 i=0;
@@ -723,6 +723,8 @@ void Group::DisbandGroup(bool alt_msg, uint32 msg) {
 		membername[i][0] = '\0';
 	}
 
+	SetLeader(nullptr);
+
 	uint32 group_id = GetID();
 
 	auto pack = new ServerPacket(ServerOP_DisbandGroup, sizeof(ServerDisbandGroup_Struct));
@@ -884,6 +886,35 @@ void Group::VerifyGroup()
 	*/
 
 	uint32 i;
+
+
+	std::string query2 = StringFormat("SELECT leadername FROM group_leaders WHERE groupid = %lu", (unsigned long)GetID());
+	auto results2 = database.QueryDatabase(query2);
+	if (!results2.Success())
+	{
+		Log(Logs::General, Logs::Error, "Error getting group leader for group %lu: %s", (unsigned long)GetID(), results2.ErrorMessage().c_str());
+		SetLeader(nullptr);
+	}
+
+	if (results2.RowCount() == 0) {
+		Log(Logs::General, Logs::Error, "Error getting group leader for group %lu: %s", (unsigned long)GetID(), results2.ErrorMessage().c_str());
+		SetLeader(nullptr);
+	}
+
+	std::string leaderName = results2.begin[0] ? results2.begin[0] : "";
+
+	//it should be safe to use GetClientByName, but Group is trying
+	//to be generic, so we'll go for general Mob
+	if (leaderName.size() > 0)
+	{
+		Mob *ourLeader = entity_list.GetMob(leaderName.c_str());
+		if (ourLeader == nullptr)
+		{	//they aren't here anymore...
+			Log(Logs::General, Logs::Group, "Leader of group %lu named '%s' has left the zone and failed verification..", (unsigned long)GetID(), leaderName.c_str());
+			SetLeader(nullptr);
+		}
+	}
+
 	for (i = 0; i < MAX_GROUP_MEMBERS; i++) 
 	{
 		if (membername[i][0] == '\0') 
