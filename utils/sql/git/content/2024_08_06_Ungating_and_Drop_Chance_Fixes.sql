91b6b162651d48cc1921c143bd8209ac7a6a56e0
ryhoneyman <ryhoneyman@gmail.com>
2024-08-06 20:57:47 +0000

    Ungating and drop updates

---
+++ utils/sql/git/content/2024_08_06_Ungating_and_Drop_Chance_Fixes.sql
@@ -0,0 +1,11 @@
+-- Change chance of revenants in hate to drop ENC epic component 'Essence of a Vampire' from 2% to 9% (item_id:10624, lootdrop_id:116166) 
+UPDATE lootdrop_entries SET chance = 9 WHERE lootdrop_id = 116166 AND item_id = 10624;
+
+-- Change male revenants from 50% to 100% probability of 9% chance 
+UPDATE loottable_entries SET probability = 100 WHERE loottable_id = 168 AND lootdrop_id = 116166;
+
+-- Ungate Thick Caustic Fluid(1595)
+UPDATE lootdrop_entries SET min_expansion = 1 WHERE lootdrop_id = 116306 AND item_id = 1595;
+
+-- Ungate an_Iksar_tomb_raider(94003)
+UPDATE spawnentry SET min_expansion = 1 WHERE spawngroupID = 222874 AND npcID = 94003;
