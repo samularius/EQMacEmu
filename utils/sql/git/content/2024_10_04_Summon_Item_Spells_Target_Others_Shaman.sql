37e72d98132eace113859c8dd3bd10c0d68e567e
Thomas Silloway <tcsillow@gmail.com>
2024-10-04 15:39:56 -0400

    Add Summon Spirit Pouch to the list of spells that can be summoned on a target for self found

---
+++ utils/sql/git/content/2024_10_04_Summon_Item_Spells_Target_Others_Shaman.sql
@@ -0,0 +1,3 @@
+UPDATE spells_new SET targettype = 5, `range` = 300 WHERE id = 272; -- Summon Spirit Pouch
+
+UPDATE spells_en SET targettype = 5, `range` = 300 WHERE id = 272; -- Summon Spirit Pouch
