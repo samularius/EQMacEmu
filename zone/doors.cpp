20adcfe3d656b392d9ff40d877138ca00db57778
Edalyn <espause0703@gmail.com>
2025-01-12 14:23:03 -0500

    Don't ask me to use words at this hour

--- zone/doors.cpp
+++ zone/doors.cpp
@@ -375,7 +375,7 @@ void Doors::HandleClick(Client* sender, uint8 trigger, bool floor_port)
 
 			if ((int)content_service.GetCurrentExpansion() != expansion)
 			{
-				sender->Message(Chat::Red, "You are unable to enter a PVP Instance that isn't part of the current instance.");
+				sender->Message(Chat::Red, "You are unable to enter a PVP Instance that isn't part of the current expansion.");
 				return;
 			}
 		}
